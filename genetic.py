ac7f7e8f1da8e2be778383e1c6d7440210525b9a
Artun <artunalpgokdag@gmail.com>
2022-11-14 23:14:42 +0300

    cx testing

--- genetic.py
+++ genetic.py
@@ -8,52 +8,6 @@ GENERATIONS = 3
 SATISFACTION = 6
 
 def ga():
-    '''
-    # Populate gen 1 random
-    solutions = np.random.randint(0,2, (N_POP,h.BIT_LENGTH)) # contains both x and y in 16 bit binary list
-
-    print(solutions)
-    
-
-    # Evaluate
-    # TODO: START LOOP
-    ranked_solutions = np.ndarray(shape=(N_POP,2), dtype=object)   # fitness value, chrm
-    print("== Solution Points ==")
-    for s  in solutions:
-        print (h.decode(s))
-        np.append(ranked_solutions, ( h.fitness(h.decode(s)), s))
-
-
-    print("\n== Ranked Values ==")
-    #ranked_solutions.sort(reverse=True)
-    for r in ranked_solutions:
-        print(r)
-
-    #Check satifaction
-    if ranked_solutions[0][0] > SATISFACTION:
-        #break
-        pass
-
-    # # Select Parents
-    # # Cross over
-    # chromosomes = []
-    # for s in ranked_solutions:
-    #     chromosomes.append(s[1])
-    
-    # for i in chromosomes:
-    #     print(i[:8])
-    # print()
-    # chromosomes = h.crossover(chromosomes)
-    
-    # for i in chromosomes:
-    #     print(i[:8])
-
-    
-    
-    # for r in ranked_solutions:
-    #     print(r[1])
-    # Mutate
-    '''
 
     # Create initial population
     pop = [np.random.randint(0, 2, h.BIT_LENGTH).tolist() for _ in range(N_POP)]
